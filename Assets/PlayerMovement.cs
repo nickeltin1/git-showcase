127fc2a6849ebfbbb7c94a5464389427022b3566
Valentine <valentyn001@gmail.com>
2023-08-28 22:10:55 +0300

    jump complete

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,15 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float _speed;
-
+    [SerializeField] private float _speed = 200;
+    [SerializeField] private float _jumpForce = 200;
 
     private Vector2 _input;
     private Rigidbody _rb;
+    private bool _jumpRequested;
 
     private void Awake()
     {
@@ -20,10 +19,19 @@ public class PlayerMovement : MonoBehaviour
     {
         _input.x = Input.GetAxis("Horizontal");
         _input.y = Input.GetAxis("Vertical");
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _jumpRequested = true;
+        }
     }
 
     private void FixedUpdate()
     {
         _rb.velocity = new Vector3(_input.x * _speed * Time.deltaTime, _rb.velocity.y, _input.y * _speed * Time.deltaTime);
+        if (_jumpRequested)
+        {
+            _rb.AddForce(Vector3.up * _jumpForce, ForceMode.VelocityChange);
+            _jumpRequested = false;
+        }
     }
 }
